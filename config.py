44e69f0f817740b78b6485ce16c3893cedc507f2
Dheeraj Gupta <dheerajgupta1902@gmail.com>
2024-12-26 12:45:33 +0530

    follower functionality added

--- config.py
+++ config.py
@@ -4,4 +4,5 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 class Config:
     SECRET_KEY = "@#fdt5jsstsysvs"
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+    POSTS_PER_PAGE = 10
     
